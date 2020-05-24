4132ecc1c754863a03bb2bfd469fe60e1aeb9429
Jared  E <jared.epps322@gmail.com>
2020-05-24 22:58:27 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,5 +1,24 @@
-class GetRequester 
-  
-  def get_response_body
-  
-end 
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+
+
+
+
+end
